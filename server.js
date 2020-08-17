874f512238c85cb454cd622e7564957d1e7123be
towtow <69576914+towtow0059332@users.noreply.github.com>
2020-08-17 13:23:56 +0700

    Initial Setup

---
+++ server.js
@@ -0,0 +1,31 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config('.env');
+}
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('connected to Mongoose'));
+
+app.use('/', indexRouter);
+
+app.listen(process.env.PORT || 3000);
